37eee95451a1763725d16331a4581d772b357989
Irene-Li <gnituyil@gmail.com>
2023-04-14 10:00:55 +0200

    Updates and preparations for submission

--- utils.py
+++ utils.py
@@ -1,141 +1,26 @@
 import numpy as np
-from scipy.optimize import root_scalar, minimize
-from scipy.signal import find_peaks
+from scipy.optimize import root_scalar, minimize, brute
 import pandas as pd
-from scipy.stats import norm
-from scipy.stats import gamma as gamma_dist
+from scipy import stats
 from matplotlib import pyplot as plt 
 
+# ==== process phase information === 
 
-# ========================================
-
-def find_defects(datasets): 
-    for dataset in datasets:
-        p = dataset['phase']
-        l = dataset['length']
-        diff =  (p[1:] - p[:-1]) % 3
-        sign = diff[0] 
-        dataset['diff'] = diff 
-        dataset['defect'] = [] 
-        for (i, d) in enumerate(diff): 
-            if d != sign: 
-                dataset['defect'].append(i+1)
-                sign = d 
-
-def find_lengths(datasets): 
-    lengths = [[], [], []]
-    lengths_wo_defects = [[], [], []]
-    for dataset in datasets: 
-        p = dataset['phase']
-        l = dataset['length']
-        d = dataset['defect']
-        for i in range(3): 
-            lengths[i].extend([x for (x, y) in zip(l, p) if y == i])
-            lengths_wo_defects[i].extend([x for (a, (x, y)) in enumerate(zip(l, p))  if (y == i and a not in d)])
-
-    return lengths, lengths_wo_defects
-
-# ==========================================
-
-def process_file(filename): 
-    # first, read in the csv files
-    dataframe = pd.read_csv(filename)
-    
-    # extract the columns we want and concatenate them together 
-    sub_dataframe = concat_columns(dataframe)
-    
-    # make list of datasets from the sub_dataframe 
-    datasets = make_datasets(sub_dataframe) 
-    
-    # filter out the segments that are too short 
-    f = lambda x: len(x['length']) > 2 
-    datasets = list(filter(f, datasets))
-    
-    return datasets 
-    
-def concat_columns(dataframe):
-    temp = {} 
-    for col in dataframe.columns: 
-        full_name = col.split('.')
-        if len(full_name) == 1: 
-            temp[col] = dataframe[col]
-        if len(full_name) == 2:
-            temp[full_name[0]] = pd.concat([temp[full_name[0]], dataframe[col]], ignore_index=True)
-    sub_dict = {k: temp[k] for k in ['Branch', 'Length', 'Phase']}
-    sub_dataframe = pd.DataFrame(sub_dict)
-    return sub_dataframe 
-    
-def make_datasets(sub_dataframe): 
-    datasets = [] 
-    flag = True
-    phase_dict = {'6': 0, '8':1, '2':2 }
-    for (i, row) in sub_dataframe.iterrows():
-        if row.isnull().all():
-            flag = True
-        else: 
-            if flag:                
-                # start a new dataset 
-                dataset = {} 
-                datasets.append(dataset)
-                dataset['ends'] = []
-                dataset['length'] = [] 
-                dataset['phase'] = [] 
-                flag = False 
-            try: 
-                dataset['length'].append(int(row['Length'].replace(',', '')))
-                dataset['phase'].append(phase_dict[row['Phase'][-1]])
-            except Exception: 
-                print('Some error occured: \n', row)
-            if isinstance(row['Branch'], str): 
-                dataset['ends'].append(row['Branch'].lower())
-    for dataset in datasets: 
-        dataset['length'] = np.array(dataset['length'])
-        dataset['phase'] = np.array(dataset['phase'])
-    return datasets  
-
-def collect_dataset(phases, breakpoints): 
-    indexed_phases = np.zeros_like(phases) 
-    indexed_phases[phases > breakpoints[0]] = 1 
-    indexed_phases[phases > breakpoints[1]] = 2 
-    
-    
-    dataset = {'length': [], 'phase': []}
-    w = 1
-    q = indexed_phases[0]
-    for p in indexed_phases[1:]: 
-        if p == q: # if same phase as previous one 
-            w += 1 
-        else: 
-            dataset['length'].append(w)
-            dataset['phase'].append(q)
-            w = 1 
-        q = p 
-
-    dataset['length'] = np.array(dataset['length'])
-    dataset['phase'] = np.array(dataset['phase'])
-    return dataset
-
-def calculate_gradients(phases):
-    z = np.exp(phases*1j) 
-    dpdx = np.imag((z[2:]-z[:-2])/2/z[1:-1])
-    return dpdx 
-
-def smooth(phases, width): 
-    z = np.exp(phases*1j)
-    z_smooth = np.convolve(z, np.ones(width), mode='valid')/width
-    theta_smooth = np.angle(z_smooth) % (2*np.pi)
-    return theta_smooth 
-
-def smooth2(phases, width): 
-    z = np.exp(phases*1j)
-    N = len(z)
+def smooth(f, width): 
+    '''
+    Average f over width. 
+    The output is of length len(f)/width
+    '''
+    N = len(f)
     length = int(np.floor(N/width))
-    z = z[:length*width]
-    z_smooth = np.mean(z.reshape((length, width)), -1)
-    theta_smooth = np.angle(z_smooth) % (2*np.pi)
-    return theta_smooth 
+    f = f[:length*width]
+    f_smooth = np.mean(f.reshape((length, width)), -1)
+    return f_smooth 
 
 def shift(phases, tol=1): 
+    '''
+    Shift the phases such that all data points are continuous 
+    '''
     z = np.exp(phases*1j) 
     diff_z = np.abs(z[1:]-z[:-1])
     diff_ang = phases[1:]-phases[:-1]
@@ -145,12 +30,6 @@ def shift(phases, tol=1):
                 phases[i+1:] += np.pi*2
             else: 
                 phases[i+1:] -= np.pi*2 
-
-def find_turning_points(phases, prominence): 
-    peaks, _ = find_peaks(phases, prominence=prominence)
-    peaks_2, _ = find_peaks(-phases, prominence=prominence)
-    return np.concatenate([peaks, peaks_2])
-
 
 # == convert between phases and stages =============
 
@@ -177,93 +56,69 @@ def period_average(x, y):
     return phase_to_stage(theta)
 
 
-# ==================================================
+# ========== Maximum Likelihood Estimation ==================
+
+def find_BB(phi, eta, grad): 
+    '''
+    Find the Brownian Bridge from phi for fixed values of eta and gradients at the boundary 
+    '''
+    L = len(phi)+1
+    m_grad = 2*np.sin(grad)/L 
+    m_eta = (2*np.sum((1-np.cos(phi))) + 2*(1-np.cos(grad)))/L
+    x = np.arange(1, L)
+    BB = np.sin(phi) - np.sin(-grad) + eta*(1-np.cos(grad)) + eta*(1-np.cos(phi))- m_grad*x - eta*m_eta*x 
+    BB[1:] += 2*eta*np.cumsum(1-np.cos(phi[:-1]))        
+    J = np.zeros((L-1, L-1))
+    diag = np.cos(phi) + eta*np.sin(phi)
+    np.fill_diagonal(J, diag)
+    J -= eta*2*np.outer(x, np.sin(phi))/L  
+    for j in range(L-1): 
+        J[j+1:, j] += 2*eta*np.sin(phi[j])
+
+    return BB, np.abs(np.linalg.det(J))
 
 
-def plot(freqs, gamma, sigma):     
-    plt.hist(freqs, bins=10, density=True, alpha=0.6, color='g')
-    xmin, xmax = plt.xlim()
-    x = np.linspace(xmin, xmax, 100)
-    p = norm.pdf(x, 0, sigma)
-    plt.plot(x, p, 'k', linewidth=2)
-    plt.show() 
+def exact_BB(omegas): 
+    BBs = [] 
+    for omega in omegas: 
+        L = len(omega)
+        x = np.arange(1, L+1)
+        W = np.cumsum(omega)
+        BB = (W - W[-1]*x/L)[:-1]
+        BBs.append(-BB)
+    return BBs 
+
+
+def get_cov(N): 
+    M = np.tril(np.ones((N, N)))[:-1, :] - (np.arange(1, N)/N)[:, np.newaxis]
+    Cov = M @ np.identity(N) @ M.T
+    return Cov 
+
+
+def traj_logpdf(BB): 
+    Cov = get_cov(len(BB)+1)
+    invCov = np.linalg.inv(Cov)
+    var = BB.T @ invCov @ BB
+    var /= len(BB)
+    return var, stats.multivariate_normal.logpdf(BB, cov=Cov*var)
+
+def cost(phi, eta, grad): 
+    BB, d = find_BB(phi, eta, grad)
+    _, b = traj_logpdf(BB)
+    ll = b + np.log(d)
+    return -np.sum(ll)
     
-def Gamma(x, gamma): 
-    return np.sin(x) + gamma*(1-np.cos(x))
+def MAP(phi): 
+    f = lambda x: cost(phi, x[0], x[1]) 
+    res = brute(f, ((-1, 1), (-np.pi, np.pi)))
+    return res
 
-def get_freqs(data, gamma):
-    N = len(data)
-    Omega = 2*gamma*np.sum(1 - np.cos(data[1:] - data[:-1]))/N
-    freqs = Omega - Gamma(data[2:]-data[1:-1], gamma) - Gamma(data[:-2]-data[1:-1],gamma)
-    return freqs 
-
-def minus_log_likelihood(data, gamma):
-    freqs = get_freqs(data, gamma)
-    std = np.sqrt(np.sum(freqs**2)/len(freqs))
-    return -np.sum(norm.logpdf(freqs, 0, std))
-
-def minus_log_prior(gamma, alpha, beta):
-    return -gamma_dist.logpdf(gamma, alpha, scale=1/beta)
-
-def MAP_sigma(data, gamma): 
-    freqs = get_freqs(data, gamma)
-    return np.sqrt(np.sum(freqs**2)/len(freqs))
-
-def MAP_gamma(data, alpha, beta): 
-    f = lambda x: minus_log_likelihood(data, x) + minus_log_prior(x, alpha, beta)
-    res = minimize(f, 0.1)
-    return res.x[0]
-
-def errors(data, map_gamma, map_sigma, alpha, beta):
-
-    def f(gamma, sigma):
-        freqs = get_freqs(data, gamma)
-        a = -np.sum(norm.logpdf(freqs, 0, sigma))
-        b = minus_log_prior(gamma, alpha, beta)
-        return a + b 
-
-    gamma_step = 1e-5
-    sigma_step = 1e-5 
-    MAP_val = f(map_gamma, map_sigma)
-    
-    test_gammas = [map_gamma+gamma_step, map_gamma, map_gamma-gamma_step]
-    test_sigmas = [map_sigma+sigma_step, map_sigma, map_sigma-sigma_step]
-    
-    test_vals = np.reshape([f(x, y) for x in test_gammas for y in test_sigmas], (3, 3))
-    hess = np.zeros((2, 2))
-    hess[0, 0] = (test_vals[0, 1] + test_vals[2, 1] - 2*test_vals[1, 1])/(gamma_step*gamma_step)
-    hess[1, 1] = (test_vals[1, 0] + test_vals[1, 2] - 2*test_vals[1, 1])/(sigma_step*sigma_step)
-    hess[1, 0] = (test_vals[2, 2] + test_vals[0, 0] - test_vals[2, 0] - test_vals[0, 2])/(4*gamma_step*sigma_step)
-    hess[0, 1] = hess[1, 0]
-    cov_mat = np.linalg.inv(hess)
-    return np.sqrt(cov_mat[0, 0]), np.sqrt(cov_mat[1, 1])
-
-
-def plot_prior_and_posterior(data, alpha, beta):
-    fig, axes = plt.subplots(1, 2, figsize=(9, 3))
-
-    f = lambda x: minus_log_prior(x, alpha, beta)
-    gammas = np.linspace(0, 1, 100)
-    axes[0].plot(gammas, list(map(f, gammas)))
-    axes[0].set_title('Prior')
-
-    f = lambda x: minus_log_likelihood(data, x) + minus_log_prior(x, alpha, beta)
-    gammas = np.linspace(0, 1, 100)
-    axes[1].set_title('Posterior')
-    axes[1].plot(gammas, list(map(f, gammas)))  
-    plt.show()   
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+def plot_cost(phi, g): 
+    N = 50 
+    etas = np.linspace(-2, 2, N)
+    c= [] 
+    for eta in etas: 
+        c.append(cost(phi, eta, grad))
+    plt.plot(etas, c)
+    plt.title('cost')
+    plt.show()
